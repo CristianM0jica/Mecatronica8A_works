7c304f60e5dd1b982eb7ea6549fcc65d22d3923c
CristianM0jica <CristianM0jica77@users.noreply.github.com>
2020-02-27 00:20:02 -0600

    Update ejercicio15.cpp

--- Programas/1p_Ejercicios de Practica_/ejercicio15.cpp
+++ Programas/1p_Ejercicios de Practica_/ejercicio15.cpp
@@ -1,12 +1,34 @@
-//15. Escribir un algoritmo que lea los datos de entrada de un archivo que solo contiene numeros y sume los numeros positivos.
-
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
-int main(){
+int lim_inf = -50;
+int lim_sup = 50;
+int suma = 0;
 
-int datos;
-int suma;
-
-return 0;
+void alaeatorio(){
+	srand(time(NULL));
+return;
 }
+
+void ciclo (){
+	alaeatorio();
+	for(int i= 0; i < lim_sup; i++)
+	{
+	int valor =lim_inf + rand()%(lim_sup + 1 - lim_inf);
+	cout<<valor<<endl;
+		if (valor > 0)
+		{
+			suma = suma  + valor;
+		}
+	}   
+cout<<"La suma de los números positivos es: "<<suma<<endl;	
+}
+
+int main(){
+	ciclo();
+return 0;
+} 
+
